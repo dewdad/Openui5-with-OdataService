1d00473b23a52f97e89bd55e867f607c63d2ff50
Avital Ben-Natan <avital.ben-natan@sap.com>
2014-08-07 22:13:58 +0300

    Dynamic routing, concepts

--- app/MyRouter.js
+++ app/MyRouter.js
@@ -16,16 +16,93 @@ sap.ui.core.routing.Router.extend("ui5app.MyRouter", {
         // Here is the routing entry point for programmatic navigation (for lack of finding a better one).
         // Check for a name in this._oRoutes object. If it does not exist, try to create a view. On successful view creation
         // build a route with the view
+        var hash = this.oHashChanger.getHash();
+        console.debug('MyRouter.navTo', {hash:hash, arguments: arguments});
         return sap.ui.core.routing.Router.prototype.navTo.call(this, sName, oParameters, bReplace || false);
     },
 
+  /**
+   * Returns a cached view for a given name or creates it if it does not yet exists
+   *
+   * @param {string} sViewName Name of the view
+   * @param {string} sViewType Type of the view
+   * @param {string} sViewId Optional view id
+   * @return {sap.ui.core.mvc.View} the view instance
+   * @public
+   * @name sap.ui.core.routing.Router#getView
+   * @function
+   */
     getView: function(){
         // Here is te routing entry for a HashChange
         // TODO: chech hash for view by convention. If view exists create a simple route and navTo it. Make use of
         // conventional dir structure that distinguishes view by feature and have the convention route load the main/master view
-        var hash = this.oHashChanger.getHash()
+        var hash = this.oHashChanger.getHash();
+        console.debug('MyRouter.getView', {hash:hash, arguments: arguments});
+
         return sap.ui.core.routing.Router.prototype.getView.apply(this, arguments);
     },
+
+  /**
+   * Returns a cached view for a given name or creates it if it does not yet exists
+   *
+   * @param {string} sViewName Name of the view
+   * @param {string} sViewType Type of the view
+   * @param {string} sViewId Optional view id
+   * @return {sap.ui.core.mvc.View} the view instance
+   * @public
+   * @name sap.ui.core.routing.Router#getView
+   * @function
+   */
+  orig_getView: function (sViewName, sViewType, sViewId) {
+  if (!sViewName) {
+    jQuery.sap.log.error("A name for the view has to be defined");
+  }
+
+  if (!this._oViews[sViewName]) {
+    var fnCreateView = function() {
+      var oViewOptions = {
+        type: sViewType,
+        viewName: sViewName
+      };
+      if (sViewId) {
+        oViewOptions.id = sViewId;
+      }
+      return sap.ui.view(oViewOptions);
+    };
+    if (this._oOwner) {
+      var that = this;
+      ManagedObject.runWithOwner(function() {
+        that._oViews[sViewName] = fnCreateView();
+      }, this._oOwner);
+    } else {
+      this._oViews[sViewName] = fnCreateView();
+    }
+    this.fireViewCreated({
+      view: this._oViews[sViewName],
+      viewName: sViewName,
+      type: sViewType
+    });
+  }
+  return this._oViews[sViewName];
+},
+
+/**
+ * Adds or overwrites a view in the viewcache of the router, the viewname serves as a key
+ *
+ * @param {string} sViewName Name of the view
+ * @param {sap.ui.core.mvc.View} oView the view instance
+ * @since 1.22
+ * @public
+ * @name sap.ui.core.routing.Router#setView
+ * @function
+ */
+orig_setView: function (sViewName, oView) {
+  if (!sViewName) {
+    jQuery.sap.log.error("A name for the view has to be defined");
+  }
+
+  this._oViews[sViewName] = oView;
+},
 
     myNavBack : function(sRoute, mData) {
         var oHistory = sap.ui.core.routing.History.getInstance();
